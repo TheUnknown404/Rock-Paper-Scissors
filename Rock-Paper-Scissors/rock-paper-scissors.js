be914fab7e5794e98340f2b2569ae50bfc2d5e3a
TheUnknown404 <99203771+TheUnknown404@users.noreply.github.com>
2023-12-10 20:24:06 +0000

    Add files via upload
    - Added a new function and parameter to shorten the code
    - cleaned up the code and added comments

--- Rock-Paper-Scissors/rock-paper-scissors.js
+++ Rock-Paper-Scissors/rock-paper-scissors.js
@@ -1,83 +1,71 @@
+// Get references to HTML elements
 let textElement1 = document.getElementById("text");
 let textElement2 = document.getElementById("results");
-
-
-
-let computerMove;
-
-function opponent() {
-    let randomNumber = Math.random();
-if (randomNumber >= 0 && randomNumber <= 1 / 3) {
-    computerMove = "rock";
-} else if (randomNumber >= 1 / 3 && randomNumber <= 2 / 3) {
-    computerMove = "paper";
-} else {
-    computerMove = "scissors";
-}
-}
-
-
-let totalWins = 0;
-let totalLosses = 0;
-let totalTies= 0;
-
-function outcome() {
-
-
-    if (result === "won") {
-        totalWins +=1;
-        document.getElementById("wins").innerHTML = totalWins;
-    } else if (result === "tied") {
-        totalTies +=1;
-        document.getElementById("ties").innerHTML = totalTies;
-    }    
-        else {totalLosses +=1;
-              document.getElementById("losses").innerHTML = totalLosses;}
-}
-
-
 let result;
 
+// Variable to store the computer's move
+let computerMove;
 
+// Function to determine the computer's move (rock, paper, or scissors)
+function opponent() {
+  let randomNumber = Math.random();
+  if (randomNumber >= 0 && randomNumber <= 1 / 3) {
+    computerMove = "rock";
+  } else if (randomNumber >= 1 / 3 && randomNumber <= 2 / 3) {
+    computerMove = "paper";
+  } else {
+    computerMove = "scissors";
+  }
+}
 
-document.getElementById("rock").addEventListener("click", () => {
-    textElement1.textContent = "You Picked Rock";
-    opponent();
-    if (computerMove === "rock") {
-        result = "tied";
-    } else if (computerMove === "paper") {
-        result = "lost"
-    }else result = "won";
-    textElement2.textContent = "you have " + result;
+// Variables to keep track of game statistics
+let totalWins = 0;
+let totalLosses = 0;
+let totalTies = 0;
 
-    outcome();
-})
+// Function to update and display game outcome statistics
+function outcome() {
+  if (result === "won") {
+    totalWins += 1;
+    document.getElementById("wins").innerHTML = totalWins;
+  } else if (result === "tied") {
+    totalTies += 1;
+    document.getElementById("ties").innerHTML = totalTies;
+  } else {
+    totalLosses += 1;
+    document.getElementById("losses").innerHTML = totalLosses;
+  }
+}
 
+// Function to handle user's choice and determine the game outcome
+function handleUserChoice(userChoice) {
+  // Display the user's choice
+  textElement1.textContent = "You Picked " + userChoice;
 
-document.getElementById("paper").addEventListener("click", () => {
-    textElement1.textContent = "You Picked Paper"
-    opponent();
-    if (computerMove === "rock") {
-        result = "won";
-    } else if (computerMove === "paper") {
-        result = "tied";
-    }else result = "lost";
-    textElement2.textContent = "you have " + result;
+  // Determine the computer's move
+  opponent();
 
-    outcome();
-})
+  // Compare user's choice with the computer's move to determine the result
+  if (computerMove === userChoice) {
+    result = "tied";
+  } else if (
+    (userChoice === "rock" && computerMove === "scissors") ||
+    (userChoice === "paper" && computerMove === "rock") ||
+    (userChoice === "scissors" && computerMove === "paper")
+  ) {
+    result = "won";
+  } else {
+    result = "lost";
+  }
 
+  // Display the game result
+  textElement2.textContent = "You have " + result;
 
+  // Update and display game outcome statistics
+  outcome();
+}
 
-document.getElementById("scissors").addEventListener("click", () => {
-    textElement1.textContent = "You Picked Scissors";
-    opponent();
-    if (computerMove === "rock") {
-        result = "lost";
-    } else if (computerMove === "paper") {
-        result = "won";
-    }else result = "tied";
-    textElement2.textContent = "you have " + result;
-
-    outcome();
-})
+// Event listeners for user's choices (rock, paper, scissors)
+document.getElementById("rock").addEventListener("click", () => handleUserChoice("rock"));
+document.getElementById("paper").addEventListener("click", () => handleUserChoice("paper"));
+document.getElementById("scissors").addEventListener("click", () => handleUserChoice("scissors"));
